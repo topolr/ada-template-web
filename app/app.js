6f362bf13c09a9763c23c946c6b29917a2cbcf0a
Jinliang <hou80houzhu@vip.qq.com>
2017-12-17 23:55:17 +0800

    pass

--- app/app.js
+++ app/app.js
@@ -50,8 +50,8 @@ let app = {
     },
     worker: {
         scope: "/",
-        regist(done) {
-            done.then(reg => {
+        beforeregist(regist) {
+            regist.then(reg => {
                 if (reg.installing) {
                     console.log('Service worker installing');
                 } else if (reg.waiting) {
